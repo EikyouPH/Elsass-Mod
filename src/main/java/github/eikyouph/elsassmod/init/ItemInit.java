36de1854e4b9c5d3ce9428b5893f3ae70e4feae8
EikyouPH <bastien.zagala@viacesi.fr>
2024-11-03 19:47:57 +0900

    ajout des propriétées du brie

--- src/main/java/github/eikyouph/elsassmod/init/ItemInit.java
+++ src/main/java/github/eikyouph/elsassmod/init/ItemInit.java
@@ -1,6 +1,7 @@
 package github.eikyouph.elsassmod.init;
 
 import github.eikyouph.elsassmod.ElsassMod;
+import github.eikyouph.elsassmod.list.FoodList;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -8,7 +9,8 @@ import net.minecraft.registry.Registry;
 public class ItemInit {
     public static final Item FARINE = register("farine", new Item(new Item.Settings()));
     public static final Item POMME_DE_TERRE_ROUGE = register("pomme_de_terre_rouge", new Item(new Item.Settings()));
-    public static final Item FROMAGE_BRIE = register("fromage_brie", new Item(new Item.Settings()));
+    public static final Item FROMAGE_BRIE = register("fromage_brie",
+            new Item(new Item.Settings().food(FoodList.FROMAGE_BRIE)));
 
     public static <T extends Item> T register(String name, T item) {
         return Registry.register(Registries.ITEM, ElsassMod.id(name), item);
--- src/main/java/github/eikyouph/elsassmod/list/FoodList.java
+++ src/main/java/github/eikyouph/elsassmod/list/FoodList.java
@@ -1,7 +1,15 @@
 package github.eikyouph.elsassmod.list;
 
 import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 
 public class FoodList {
-
+    public  static final FoodComponent FROMAGE_BRIE = new FoodComponent.Builder()
+            .nutrition(5)
+            .saturationModifier(0.4F)
+            .alwaysEdible()
+            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 20 * 30, 1), 0)
+            .build();
 }
