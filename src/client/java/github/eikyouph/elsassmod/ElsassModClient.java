b2ecb5713ba009064bd0ebc7133ee967bed2ddfa
EikyouPH <bastien.zagala@viacesi.fr>
2024-11-03 16:47:26 +0900

    ajout de git

---
+++ src/client/java/github/eikyouph/elsassmod/ElsassModClient.java
@@ -0,0 +1,10 @@
+package github.eikyouph.elsassmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElsassModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/github/eikyouph/elsassmod/ElsassModDataGenerator.java
@@ -0,0 +1,11 @@
+package github.eikyouph.elsassmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ElsassModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
