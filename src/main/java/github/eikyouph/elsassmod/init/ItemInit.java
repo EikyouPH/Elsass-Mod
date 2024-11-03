d30ca63d530ef27817fcbb2df3890e126c706f9e
EikyouPH <bastien.zagala@viacesi.fr>
2024-11-03 19:48:37 +0900

    modif des propriétées du brie

--- src/main/java/github/eikyouph/elsassmod/init/ItemInit.java
+++ src/main/java/github/eikyouph/elsassmod/init/ItemInit.java
@@ -10,7 +10,7 @@ public class ItemInit {
     public static final Item FARINE = register("farine", new Item(new Item.Settings()));
     public static final Item POMME_DE_TERRE_ROUGE = register("pomme_de_terre_rouge", new Item(new Item.Settings()));
     public static final Item FROMAGE_BRIE = register("fromage_brie",
-            new Item(new Item.Settings().food(FoodList.FROMAGE_BRIE)));
+            new Item(new Item.Settings().food(FoodList.FROMAGE_BRIE).maxCount(16)));
 
     public static <T extends Item> T register(String name, T item) {
         return Registry.register(Registries.ITEM, ElsassMod.id(name), item);
