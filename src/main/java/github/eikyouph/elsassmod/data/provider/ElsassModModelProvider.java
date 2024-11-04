a8080915607c97ec032336829bb6d4662598a8ef
EikyouPH <bastien.zagala@viacesi.fr>
2024-11-04 15:20:12 +0900

    Ajouts de graines supplémentaires

--- src/main/java/github/eikyouph/elsassmod/data/provider/ElsassModModelProvider.java
+++ src/main/java/github/eikyouph/elsassmod/data/provider/ElsassModModelProvider.java
@@ -41,5 +41,23 @@ public class ElsassModModelProvider extends FabricModelProvider {
         itemModelGenerator.register(ItemInit.ANISE_SEED, Models.GENERATED);
         itemModelGenerator.register(ItemInit.CINNAMON_SEED, Models.GENERATED);
         itemModelGenerator.register(ItemInit.CHOCOLATE_POWDER, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.LEEK_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.RUTABAGA_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.SMALL_PUMPKIN_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.PARSLEY_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.CHIVES_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.JUNIPER_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.SHALLOT_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.BARLEY_MALT, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.BARLEY_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.HOP_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.PLUM_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.RHUBARB_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.ORANGE_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.LEMON_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.CLOVE_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.CELERY_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.TURNIP_SEED, Models.GENERATED);
+        itemModelGenerator.register(ItemInit.LAUREL_SEED, Models.GENERATED);
     }
 }
--- src/main/java/github/eikyouph/elsassmod/init/ItemInit.java
+++ src/main/java/github/eikyouph/elsassmod/init/ItemInit.java
@@ -26,6 +26,24 @@ public class ItemInit {
     public static final Item ANISE_SEED = register("anise_seed", new Item(new Item.Settings()));
     public static final Item CINNAMON_SEED = register("cinnamon_seed", new Item(new Item.Settings()));
     public static final Item CHOCOLATE_POWDER = register("chocolate_powder", new Item(new Item.Settings()));
+    public static final Item LEEK_SEED = register("leek_seed", new Item(new Item.Settings()));
+    public static final Item RUTABAGA_SEED = register("rutabaga_seed", new Item(new Item.Settings()));
+    public static final Item SMALL_PUMPKIN_SEED = register("small_pumpkin_seed", new Item(new Item.Settings()));
+    public static final Item PARSLEY_SEED = register("parsley_seed", new Item(new Item.Settings()));
+    public static final Item CHIVES_SEED = register("chives_seed", new Item(new Item.Settings()));
+    public static final Item JUNIPER_SEED = register("juniper_seed", new Item(new Item.Settings()));
+    public static final Item SHALLOT_SEED = register("shallot_seed", new Item(new Item.Settings()));
+    public static final Item BARLEY_MALT = register("barley_malt", new Item(new Item.Settings()));
+    public static final Item HOP_SEED = register("hop_seed", new Item(new Item.Settings()));
+    public static final Item BARLEY_SEED = register("barley_seed", new Item(new Item.Settings()));
+    public static final Item PLUM_SEED = register("plum_seed", new Item(new Item.Settings()));
+    public static final Item RHUBARB_SEED = register("rhubarb_seed", new Item(new Item.Settings()));
+    public static final Item ORANGE_SEED = register("orange_seed", new Item(new Item.Settings()));
+    public static final Item LEMON_SEED = register("lemon_seed", new Item(new Item.Settings()));
+    public static final Item CLOVE_SEED = register("clove_seed", new Item(new Item.Settings()));
+    public static final Item CELERY_SEED = register("celery_seed", new Item(new Item.Settings()));
+    public static final Item TURNIP_SEED = register("turnip_seed", new Item(new Item.Settings()));
+    public static final Item LAUREL_SEED = register("laurel_seed", new Item(new Item.Settings()));
 
     public static <T extends Item> T register(String name, T item) {
         return Registry.register(Registries.ITEM, ElsassMod.id(name), item);
